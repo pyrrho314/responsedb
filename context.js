f1a8fe2b9e32dc8379f3e4caaf1caae4225dbf3d
callen314 <callen314@gmail.com>
2014-05-27 15:47:46 -0400

    added context

---
+++ context.js
@@ -0,0 +1,15 @@
+var n9context  = "comment_bearing";
+
+var href = window.location.href;
+
+if (href.indexOf("apis.google.com") >=0) 
+{
+    n9context = "comment_iframe";
+}
+
+if (href.indexOf("chrome-extension:") >=0)
+{
+    n9context = "background";
+}
+
+console.log("context.js: n9context =", n9context);
